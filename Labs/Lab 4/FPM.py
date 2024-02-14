657dcb19d380edd7936970eace7cbc8762d7f934
unknown <gavin.shields@colorado.edu>
2024-02-13 21:46:51 -0700

    Pre lab code

---
+++ Labs/Lab 4/FPM.py
@@ -0,0 +1,50 @@
+# import libraries
+import numpy as np
+def driver():
+# test functions
+    f1 = lambda x: 1+0.5*np.sin(x)
+# fixed point is alpha1 = 1.4987....
+    f2 = lambda x: 3+2*np.sin(x)
+#fixed point is alpha2 = 3.09...
+    f3 = lambda x: (10/(x+4))**(1/2)
+#fixed point is alpha2 = 3.09...
+    Nmax = 100
+    tol = 10e-10
+# test f1 '''
+  #  x0 = 0.0
+  #  [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+  #  print('the approximate fixed point is:',xstar)
+  #  print('f1(xstar):',f1(xstar))
+   # print('Error message reads:',ier)
+#test f2 '''
+   # x0 = 0.0
+   # [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+   # print('the approximate fixed point is:',xstar)
+   # print('f2(xstar):',f2(xstar))
+   # print('Error message reads:',ier)
+#test f3 '''
+    x0 = 1.5
+    count = 0
+    [xstar,ier, count1] = fixedpt(f3,x0,tol,Nmax, count)
+    print('the approximate fixed point is:',xstar)
+    print('f3(xstar):',f3(xstar))
+    print('Error message reads:',ier)
+    print(count)
+# define routines
+def fixedpt(f,x0,tol,Nmax, count):
+    ''' x0 = initial guess'''
+    ''' Nmax = max number of iterations'''
+    ''' tol = stopping tolerance'''
+    #count = 0
+    while (count <Nmax):
+      count = count +1
+      x1 = f(x0)
+      if (abs(x1-x0) <tol):
+        xstar = x1
+        ier = 0
+        return [xstar,ier, count]
+      x0 = x1
+    xstar = x1
+    ier = 1
+    return [xstar, ier, count]
+driver()
